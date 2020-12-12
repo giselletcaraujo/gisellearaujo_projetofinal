9b484289d9e4af806bec2f8b81516dcfa755bf35
giselletcaraujo <giselletcaraujo@gmail.com>
2020-12-12 15:31:00 -0300

    commit 2 mongoose

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app")
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Servidor está rodando na porta ${port}`)
-})
+    console.log(`Servidor está rodando na porta ${port}`);
+});
